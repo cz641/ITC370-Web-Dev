e043582e4b0d9e5d2b5ca0610d5ce1eb68211f5b
cz641 <89827173+cz641@users.noreply.github.com>
2021-11-18 22:24:50 -0600

    Delete scripts.js

--- Bootstrap, CSS and JS Lab/scripts.js
+++
@@ -1,2 +0,0 @@
-//document.write("<footer class='text-light bg-dark'>Thank you again!</footer> ");
-document.getElementById("theFooter").innerHTML = "<footer class='text-light bg-dark'>Thank you again!</footer>";
